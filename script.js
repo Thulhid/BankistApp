3da45ab439785ba2e5aba6f4dc998fe1f4db716d
Thulhid <153322849+Thulhid@users.noreply.github.com>
2024-09-27 11:39:23 +0530

    implementing request loan

--- script.js
+++ script.js
@@ -184,6 +184,26 @@ btnLogin.addEventListener('click', function (e) {
     }
   });
 
+  //Idea: some
+
+  btnLoan.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const amount = Number(inputLoanAmount.value);
+    if (
+      amount > 0 &&
+      currentAccount.movements.some(mov => mov >= amount * 0.1)
+    ) {
+      //add account
+      currentAccount.movements.push(amount);
+
+      //update UI
+      updateUI(currentAccount);
+    }
+
+    inputLoanAmount.value = '';
+  });
+
   btnClose.addEventListener('click', function (e) {
     e.preventDefault();
     if (
